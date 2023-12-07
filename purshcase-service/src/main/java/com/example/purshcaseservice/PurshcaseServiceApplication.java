78bb5f57775653815cc738267008977447705943
Миша Вагин <mihail.vagin.10@mail.ru>
2023-12-07 20:11:41 +0300

    initial

---
+++ purshcase-service/src/main/java/com/example/purshcaseservice/PurshcaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.purshcaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurshcaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurshcaseServiceApplication.class, args);
+	}
+
+}
---
+++ purshcase-service/src/test/java/com/example/purshcaseservice/PurshcaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purshcaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurshcaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
