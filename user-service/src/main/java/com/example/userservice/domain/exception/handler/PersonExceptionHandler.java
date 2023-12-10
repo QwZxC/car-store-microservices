d9adfba8986ef71bdf7cbc05f7a2a21f71cd4224
mihai <mihai@HOMEPC>
2023-12-10 14:54:41 +0300

    Person-service: exception handling

--- user-service/src/main/java/com/example/userservice/domain/exception/handler/PersonExceptionHandler.java
+++ user-service/src/main/java/com/example/userservice/domain/exception/handler/PersonExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.example.userservice.domain.exception.handler;
 
+import com.example.userservice.domain.exception.ErrorResponse;
+import com.example.userservice.domain.exception.types.PersonNotFoundException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,9 +15,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class PersonExceptionHandler  extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(PersonExceptionHandler.class)
+    @ExceptionHandler(PersonNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(PersonExceptionHandler ex) {
+    public ResponseEntity<ErrorResponse> handleNotFoundException(PersonNotFoundException ex) {
         ErrorResponse response = new ErrorResponse(
                 HttpStatus.NOT_FOUND,
                 ex.getMessage()
